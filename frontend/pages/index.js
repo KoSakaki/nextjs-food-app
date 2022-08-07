b522c12e627b34d4297b9372e6f57a2683080c8a
KoSakaki <sakaki@flaginc.net>
2022-08-07 22:30:35 +0900

    first commit

---
+++ frontend/pages/index.js
@@ -0,0 +1,14 @@
+import {Alert, Button} from "reactStrap";
+
+const index = () => {
+  return (
+    <div>
+      <div>
+        <Alert color="primary">Hello </Alert>
+        <Button className="primary">Hello </Button>
+      </div>
+    </div>
+  );
+}
+
+export default index;
